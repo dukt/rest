e8add347eba420beaad46635bf998aa69f600aa2
Benjamin David <ben@dukt.net>
2014-11-22 23:26:40 +0100

    Added Linked In API

---
+++ rest/src/Api/Linkedin.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+* Craft REST by Dukt
+ *
+ * @package   Craft REST
+ * @author    Benjamin David
+ * @copyright Copyright (c) 2014, Dukt
+ * @link      https://dukt.net/craft/rest/
+ * @license   https://dukt.net/craft/rest/docs#license
+ */
+
+namespace Dukt\Rest\Api;
+
+class Linkedin extends AbstractApi {
+
+    public function getName()
+    {
+        return "LinkedIn";
+    }
+
+    public function getProviderHandle()
+    {
+        return 'linkedin';
+    }
+
+    public function getApiUrl()
+    {
+        return 'https://api.linkedin.com/v1/'; // /people/~?format=json
+    }
+
+    public function getScopes()
+    {
+        return array(
+            'r_basicprofile',
+            'r_fullprofile',
+            'r_emailaddress',
+            'r_network',
+            'r_contactinfo',
+            'rw_nus',
+            'rw_company_admin',
+            'rw_groups',
+            'w_messages',
+        );
+    }
+
+    public function getAuthorizationMethod()
+    {
+        return 'oauth2_access_token';
+    }
+}
